41ec28925fc56f477ca93111e6d425450ea891e9
David Fulton <david.fulton@lifestylepubs.com>
2020-11-22 15:43:19 -0600

    Refactored foo to index

--- templates/typescript/src/__tests__/index.spec.ts
+++ templates/typescript/src/__tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import { sum } from '../foo';
+import { sum } from '../index';
 
 test('basic', () => {
   expect(sum()).toBe(0);
--- templates/typescript/src/index.ts
+++ templates/typescript/src/index.ts
@@ -1,4 +1,6 @@
 const chalk = require('chalk');
 const boxen = require('boxen');
 
+export const sum = (...a: number[]) => a.reduce((acc, val) => acc + val, 0);
+
 console.log(boxen(chalk.blue('Hello world!'), { padding: 1, margin: 1 }));

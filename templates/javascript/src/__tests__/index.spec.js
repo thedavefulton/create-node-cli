33e96d79c74cd569517f3b6fc9934f8b148318f7
David Fulton <david.fulton@lifestylepubs.com>
2020-11-23 17:37:26 -0600

    Refactored to commonjs

--- templates/javascript/src/__tests__/index.spec.js
+++ templates/javascript/src/__tests__/index.spec.js
@@ -1,4 +1,4 @@
-import { sum } from '../index';
+const { sum } = require('../index');
 
 test('sum', () => {
   expect(sum()).toBe(0);
--- templates/javascript/src/index.js
+++ templates/javascript/src/index.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 const boxen = require('boxen');
 
-export const sum = (...a) => a.reduce((acc, val) => acc + val, 0);
+const sum = (...a) => a.reduce((acc, val) => acc + val, 0);
 
 console.log(boxen(chalk.blue('Hello world!'), { padding: 1, margin: 1 }));
+
+module.exports = { sum };
